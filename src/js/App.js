3ee3501e4c2365abe743c293337c37b13f291144
LeNgocPhuc99 <lphuc5461@gmail.com>
2021-12-22 21:33:14 +0700

    display task list

--- src/js/App.js
+++ src/js/App.js
@@ -4,9 +4,9 @@ import TodoList from "../abis/TodoList.json";
 import TaskList from "./components/TaskList";
 import "../css/App.css";
 
+
 function App(props) {
   const [account, setAccount] = useState();
-  const [network, setNetwork] = useState({ id: "0" });
   const [todoListContract, setTodoListContract] = useState("");
   const [taskCount, setTaskCount] = useState(0);
   const [tasks, setTasks] = useState([]);
@@ -29,7 +29,6 @@ function App(props) {
       setAccount(accounts[0]);
       // load users network ID and name
       const networkId = await web3.eth.net.getId();
-      setNetwork({ ...network, id: networkId });
 
       const todoListData = TodoList.networks[networkId];
       if (todoListData) {
@@ -46,7 +45,7 @@ function App(props) {
         // load tasks list
         for (let i = 1; i <= taskCount; i++) {
           const task = await todoListContract.methods.tasks(i).call();
-          setTasks(...tasks, task);
+          setTasks(tasks => [...tasks, task]);
         }
       } else {
         setAppStatus(false);
--- src/js/components/TaskList.js
+++ src/js/components/TaskList.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 
 const TaskList = (props) => {
-  const [taskContent, setTaskContent] = useState();
+  const [taskContent, setTaskContent] = useState("");
+
+  const inputChangeHandler = (event) => {
+    event.preventDefault();
+    setTaskContent(event.target.value);
+  };
+
   return (
     <div id="content">
       <form
@@ -12,14 +18,30 @@ const TaskList = (props) => {
       >
         <input
           id="newTask"
-          ref={(input) => {
-            setTaskContent(input);
-          }}
           type="text"
-        ></input>
+          placeholder="Add task..."
+          onChange={inputChangeHandler}
+          value={taskContent}
+        />
         <input type="submit" hidden={true} />
       </form>
-
+      <ul id="taskList" className="list-unstyled">
+        {props.tasks.map((task, key) => {
+          return (
+            <div className="taskTemplate" className="checkbox" key={key}>
+              <label>
+                <input
+                  type="checkbox"
+                  name={task.id}
+                  defaultChecked={task.done}
+                />
+                <span className="content">{task.content}</span>
+              </label>
+            </div>
+          );
+        })}
+      </ul>
+      <ul id="completedTaskList" className="list-unstyled"></ul>
     </div>
   );
 };
